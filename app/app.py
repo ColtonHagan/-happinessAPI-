5edc8eafeda878a8543c0641974e1966e0d3e8d7
myusername <myemail>
2022-06-23 23:45:16 -0700

    finished needs polish and documentation

--- app/app.py
+++ app/app.py
@@ -1,36 +1,75 @@
 from flask import Flask
-from flask_restful import Api, Resource, abort
+from flask_restful import Api, Resource, abort, reqparse
 import os
 import mysql.connector
 
 app = Flask(__name__)
 api = Api(app)
 
-#
-def databaseCall(sql):
-    #TODO add error check to see if can connect to database using try
-    connection = mysql.connector.connect(user='root', password='root', host='mysql', port='3306', database='db')
+#Parses args when putting new data
+happiness_put_arg = reqparse.RequestParser()
+happiness_put_arg.add_argument("happiness_index", type=float, help="Please include the happiness_index value", required=True)
+
+#Creates server connection with database that has given information
+def create_server_connection(username, password_name, host_name, port_name, db):
+    connection = None
+    try:
+        connection = mysql.connector.connect(user=username,
+            password=password_name,
+            host=host_name,
+            port=port_name, 
+            database=db)
+    except Error as err:
+        abort(400, error=err)
+    return connection
+
+#Performs a safe sql call using given sql command
+#if fetching is true returns fetched data, otherwise commits
+def database_call(sql,var,fetching):
+    output = None
+    connection = create_server_connection('root', 'root', 'mysql', '3306', 'db')
     cursor = connection.cursor()
-    cursor.execute(sql)
-    output = cursor.fetchall()
+    cursor.execute(sql,var)
+    if(fetching): output = cursor.fetchall()
+    else: connection.commit()
     connection.close()
     return output
 
-#Get happiness indice by country id
+#Get/posts happiness index by country id
 class Happiness(Resource):
     def get(self, country_id):
-        happiness = databaseCall("select HappinessIndex FROM happiness WHERE CountryId = " + str(country_id))
+        happiness = database_call("SELECT HappinessIndex FROM happiness WHERE CountryId = %s", (country_id,), True)
         if(not happiness): abort(400, error="Country id is not in database")
         return {"happiness": float(happiness[0][0])}
-api.add_resource(Happiness,"/by_id/<int:country_id>")
 
-#TODO Get average happiness of all countries in database 
+    def put(self, country_id):
+        if(database_call("SELECT HappinessIndex FROM happiness WHERE CountryId = %s", (country_id,),True)):
+            abort(400, error="Country id is already in database")
+        happiness_index = happiness_put_arg.parse_args()["happiness_index"]
+
+        #if entered value has more then one decimal place throws error
+        if(len(str(happiness_index).split(".")[1]) > 1): 
+            abort(400, error="Happiness_index should only have a number to one decimal place")
+        values = (country_id, happiness_index)
+        database_call("INSERT INTO happiness (CountryId, HappinessIndex) VALUES (%s, %s)", values, False)
+        return {"Sucesss": country_id}
+
+#Get average happiness and related statisics for happiness index of all countries in database
 class Average(Resource):
     def get(self):
-        #Error check if it exists 
-        #input check correct input
-        mean = databaseCall("select Avg(HappinessIndex) FROM happiness")
-        return {"Mean": float(mean[0][0])}
+        mean = database_call("SELECT AVG(HappinessIndex) FROM happiness",(), True)
+        mode = database_call("SELECT HappinessIndex FROM happiness GROUP BY HappinessIndex " +
+                             "ORDER BY count(HappinessIndex) DESC LIMIT 1", (), True)
+        maxHappy = database_call("SELECT MAX(HappinessIndex) FROM happiness", (), True)
+        minHappy = database_call("SELECT MIN(HappinessIndex) FROM happiness", (), True)
+        return {"Mean": round(float(mean[0][0]),1), 
+                "Mode": round(float(mode[0][0]),1),
+                "Max": round(float(maxHappy[0][0]),1),
+                "Min": round(float(minHappy[0][0]),1),
+                "Range": round(float((maxHappy[0][0] - minHappy[0][0])),1)
+                }
+
+api.add_resource(Happiness,"/by_id/<int:country_id>")
 api.add_resource(Average,"/average")
 
 if __name__ == "__main__":
--- example.py
+++ example.py
@@ -1,11 +1,20 @@
-#TEMP testing for primative version of api
+#Examples of API working
 import requests
 
 BASE_URL = "http://127.0.0.1:5000/"
 
-#Testing api
-response = requests.get(BASE_URL)
+response = requests.get(BASE_URL + "by_id/1")
 print(response.json())
+
+response = requests.put(BASE_URL + "by_id/1", {"happiness_index": 1.0})
+print(response.json())
+
+response = requests.get(BASE_URL + "by_id/1")
+print(response.json())
+
+#Testing api
+#response = requests.get(BASE_URL)
+#print(response.json())
 
 #Should work
 #response = requests.get(BASE_URL + "by_id/12345")
@@ -14,3 +23,4 @@ print(response.json())
 #Should fail
 #response = requests.get(BASE_URL + "by_id/2")
 #print(response.json())
+
